a82b92bb169581cdc3bccf91ede917f4c5a315ce
sarah-code <trancansar@outlook.com>
2019-12-01 15:44:49 +0100

    Minor edits

--- models/board_of_ideas.py
+++ models/board_of_ideas.py
@@ -1,18 +1,27 @@
-
-
 from odoo import models, fields, api
+from datetime import datetime
 
 class board_of_ideas(models.Model):
     _name = 'board.of.ideas'
 
-    date = fields.Date(string="Date")
+    title = fields.Text(string="Title")
+
+    date = fields.Date(string="Date", default=datetime.today())
     idea = fields.Text(string="Idea/Area of Improvement")
     dep = fields.Text(string="department")
-    login = fields.Many2one(string="Login", comodel_name="res.partner")
+    login = fields.Many2one(string="Login", comodel_name="res.partner", default=lambda self: self.env.uid)
 
     owner = fields.Many2one(string="Owner", comodel_name="res.partner")
     resp_date = fields.Date(string="Response date")
     resp = fields.Text(string="Response")
     state = fields.Selection(string="Status", selection=[('draft','Draft'),('proposed','Proposed'),('onit','Working on'),('implemented','Implemented'),('rejected','Rejected')], default="draft")
     int_notes = fields.Html(string="Internal Notes")
+
+    def name_get(self):
+        data = []
+        for rec in self:
+            title = "ISSUE-%s" % (rec.id)
+            data.append((rec.id, title))
+        return data
+
 
