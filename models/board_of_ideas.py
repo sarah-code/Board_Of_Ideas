65d69c96621587550c79815e2ff05c4e7347ac57
sarah-code <trancansar@outlook.com>
2019-12-18 22:12:13 +0100

    added mail mixins with oe_chatter

--- models/board_of_ideas.py
+++ models/board_of_ideas.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 class board_of_ideas(models.Model):
     _name = 'board.of.ideas'
+    _inherit = ['mail.thread','mail.activity.mixin']
 
     title = fields.Text(string="Title", store ="True", required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'))
 
@@ -11,11 +12,13 @@ class board_of_ideas(models.Model):
     dep = fields.Text(string="Department")
     login = fields.Many2one(string="Login", comodel_name="res.partner", store="True", default=lambda self: self.env.user.id)
 
-    owner = fields.Many2many(string="Owner", comodel_name="res.partner")
+    owners = fields.Many2many(string="Owner(s)", comodel_name="res.partner")
     resp_date = fields.Date(string="Response date")
     resp = fields.Text(string="Response")
     state = fields.Selection(string="Status", selection=[('draft','Draft'),('proposed','Proposed'),('onit','Working on'),('implemented','Implemented'),('rejected','Rejected')], default="draft")
     int_notes = fields.Html(string="Internal Notes")
+
+
 
     def name_get(self):
         data = []
