0b22c88e260311d7f6d25c6cc53d1a37b8ac4c6d
sarah-code <trancansar@outlook.com>
2019-12-19 21:38:56 +0100

    added severnity field, statusbar, login name fixed

--- models/board_of_ideas.py
+++ models/board_of_ideas.py
@@ -7,18 +7,28 @@ class board_of_ideas(models.Model):
 
     title = fields.Text(string="Title", store ="True", required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'))
 
+    sev = fields.Selection(string="Severnity", selection=[('0','O'),('1','!'),('2','!!'),('3','!!!'),('4','!!!!')], default="0", help='How to rate severnity?\n o means no immediate attention needed.\n ! means attention needed in a year.\n !! means attention needed in 6 months.\n !!! means attention needed in 3 months. \n !!!! means attention highly need in under 3 months.')
     date = fields.Date(string="Date", default=datetime.today())
     idea = fields.Text(string="Idea/Area of Improvement")
     dep = fields.Text(string="Department")
-    login = fields.Many2one(string="Login", comodel_name="res.partner", store="True", default=lambda self: self.env.user.id)
+    login = fields.Many2one(string="Login", comodel_name="res.users", store="True", default=lambda self: self.env.user.id)
 
-    owners = fields.Many2many(string="Owner(s)", comodel_name="res.partner")
+    owners = fields.Many2many(string="Owner(s)", comodel_name="res.users")
     resp_date = fields.Date(string="Response date")
     resp = fields.Text(string="Response")
     state = fields.Selection(string="Status", selection=[('draft','Draft'),('proposed','Proposed'),('onit','Working on'),('implemented','Implemented'),('rejected','Rejected')], default="draft")
     int_notes = fields.Html(string="Internal Notes")
 
-
+    @api.onchange('sev')
+    def severnity_warning(self):
+        for rec in self:
+            if int(rec.sev) >= 3:
+                return {
+                   'warning': {
+                        'title': _('Severnity level warning'),
+                        'message': _('You are about to log an issue with severnity "'+ dict(self._fields['sev'].selection).get(self.sev) +' - ' + self.sev + '".\n Please be rethink how severe the issue really is or talk to you manager.')
+                   }
+                }
 
     def name_get(self):
         data = []
@@ -33,7 +43,6 @@ class board_of_ideas(models.Model):
             vals['title'] = self.env['ir.sequence'].next_by_code('code')
         res = super().create(vals)
         return res
-
 
 
 
